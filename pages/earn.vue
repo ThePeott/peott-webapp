58a6e08f96379390c288d3e0ff7704409d951d90
qu-cipher <ali.code88s@gmail.com>
2025-02-27 14:52:09 +0330

    add earn & frens pages

---
+++ pages/earn.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <h1>Earn page</h1>
+</template>
+
+<style>
+
+h1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ pages/frens.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <h1>Frens page</h1>
+</template>
+
+<style>
+h1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
